448ab38e8a1eddcf9140c028bcb9e11dcba20bda
cmehadji27 <cmehadji27@gmail.com>
2025-04-11 10:14:52 +0200

    read csv

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,16 @@ import org.apache.spark.sql.SparkSession
 object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .appName("TP1 Spark Scala")
+      .appName("TP1 Spark Scala - Lecture CSV")
       .master("local[*]")
       .getOrCreate()
 
-    val data = Seq("Scala", "Spark", "TP1")
-    val df = spark.createDataFrame(data.map(Tuple1(_))).toDF("value")
+    val csvPath = "seattle-weather.csv"
+
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(csvPath)
 
     df.show()
 
